9ec7076590ecbaca6938f5912dc42449b260054a
trungthanhbmtran <52433210+trungthanhbmtran@users.noreply.github.com>
2022-08-24 09:50:43 +0700

    WorkDone
    update

--- components/indexs/card/index.js
+++ components/indexs/card/index.js
@@ -6,8 +6,8 @@ import Carousel from 'react-material-ui-carousel';
 
 const items = [
     {
-        Name: "Electronics",
-        Caption: "Electrify your friends!",
+        Name: "Điện mặt trời TNHH Gia Thành",
+        Caption: "Địa chỉ tại cư mgar",
         contentPosition: "left",
         Items: [
             {
@@ -19,14 +19,14 @@ const items = [
                 Image: "https://source.unsplash.com/featured/?iphone"
             },
             {
-                Name: "iPhone chi tiet",
+                Name: "iPhone",
                 Image: "https://source.unsplash.com/featured/?iphone"
             }
         ]
     },
     {
-        Name: "Home Appliances",
-        Caption: "Say no to manual home labour!",
+        Name: "Hệ thống NLMT của Công ty CP Đầu tư và Phát triển Năng Lượng Cao Nguyên ",
+        Caption: "huyện Buôn Đôn – tỉnh Đăk Lăk với công suất 5 MBW",
         contentPosition: "middle",
         Items: [
             {
@@ -44,8 +44,8 @@ const items = [
         ]
     },
     {
-        Name: "Decoratives",
-        Caption: "Give style and color to your living room!",
+        Name: "Thí nghiệm định kỳ nhà máy thủy điện Sereepok 4A – của Công ty CP Thủy điện Buôn Đôn ",
+        Caption: "huyện Buôn đôn -tỉnh Đăk Lăk",
         contentPosition: "right",
         Items: [
             {
@@ -93,27 +93,28 @@ const ActionAreaCard = () => {
 }
 
 const Banner = (props) => {
-    const totalItems =   props.length ? props.length : 3;
+    const contentPosition = props.contentPosition ? props.contentPosition : "left"
+    const totalItems =   props.length ? props.length : 2;
     const mediaLength = totalItems ;
     let items = [];
 
 
-    // const content = (
-    //     <Grid item xs={4} key="content">
-    //         <CardContent className="Content">
-    //             <Typography className="Title">
-    //                 {props.item.Name}
-    //             </Typography>
-    //             <Typography className="Caption">
-    //                 {props.item.Caption}
-    //             </Typography>
-    //             <Button variant="outlined" className="ViewButton">
-    //                 View Now
-    //             </Button>
-    //         </CardContent>
-    //         {/* <CardComponent tittle={props.item.Name}></CardComponent> */}
-    //     </Grid>
-    // )
+    const content = (
+        <Grid item xs={4} key="content">
+            <CardContent className="Content">
+                <Typography className="Title">
+                    {props.item.Name}
+                </Typography>
+                <Typography className="Caption">
+                    {props.item.Caption}
+                </Typography>
+                <Button variant="outlined" className="ViewButton">
+                    Xem chi tiết
+                </Button>
+            </CardContent>
+            {/* <CardComponent tittle={props.item.Name}></CardComponent> */}
+        </Grid>
+    )
 
     for (let i = 0; i < mediaLength; i++) {
         const item = props.item.Items[i];
@@ -128,12 +129,7 @@ const Banner = (props) => {
                     <Typography className="MediaCaption" color='Yellow'>
                         {item.Name}
                     </Typography>
-                    <Button variant="contained" className="ViewButton" onClick={() => alert(item.Name)}>
-                       Chi tiết
-                    </Button>
                 </CardMedia>
-               
-                
                 {/* <CardComponent tittle={props.item.Name} ></CardComponent> */}
             </Grid>
         )
@@ -141,13 +137,13 @@ const Banner = (props) => {
         items.push(media);
     }
 
-    // if (contentPosition === "left") {
-    //     items.unshift(content);
-    // } else if (contentPosition === "right") {
-    //     items.push(content);
-    // } else if (contentPosition === "middle") {
-    //     items.splice(items.length / 2, 0, content);
-    // }
+    if (contentPosition === "left") {
+        items.unshift(content);
+    } else if (contentPosition === "right") {
+        items.push(content);
+    } else if (contentPosition === "middle") {
+        items.splice(items.length / 2, 0, content);
+    }
 
     return (
         <Card raised className="Banner">
