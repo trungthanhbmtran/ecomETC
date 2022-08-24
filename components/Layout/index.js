967a17542abbbca1c878d1c127bcb36e1ff35d94
trungthanhbmtran <52433210+trungthanhbmtran@users.noreply.github.com>
2022-08-24 22:14:16 +0700

    update layout
    fix layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -6,7 +6,7 @@ import AnimationIcon from './AnimationIcon';
 import IconTool from './IconFB';
 import Footer from './Footer';
 import BackToTop from './ScrollOnTop';
-import { Container } from '@mui/system';
+import { Box } from '@mui/system';
 import Header from './Header';
 import AppBar from './AppBar';
 import DrawerAppBar from './Drawer';
@@ -15,18 +15,16 @@ import DrawerAppBar from './Drawer';
 export default function Layout({children}) {
 
   return (
-    <Container maxWidth="false" >
+    <Box maxWidth="false" >
       <div id="back-to-top-anchor" />
       <Header/>
-      <List sx={{ mb: 2 }}>
              <AppBar/>
               <DrawerAppBar />
                 {children}
              <BackToTop/>
-      </List>
       <Footer/>
       <AnimationIcon />
       <IconTool />
-    </Container>
+    </Box>
   );
 }
